6defe2f15a48637d56b046fad7b3c803a9956363
Maurits Seelen <mauritsseelen@hotmail.com>
2024-03-24 17:38:13 +0100

    lexer

---
+++ src/lexer.rs
@@ -0,0 +1,109 @@
+#[derive(Debug)]
+pub enum Token {
+    Float(f64),
+    Int(i32),
+    String,
+    LefTParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    LeftBracket,
+    RightBracket,
+    Nil,
+    And,
+    Or,
+    For,
+    While,
+    If,
+    Else,
+    Return,
+    Invalid(String),
+    Eof,
+}
+
+pub struct Lexer<'a> {
+    code: std::iter::Peekable<std::str::Chars<'a>>,
+    line: i8,
+    tokens: Vec<Token>,
+}
+
+impl<'a> Lexer<'a> {
+    pub fn new(code: &'a str) -> Self {
+        Self {
+            code: code.chars().peekable(),
+            line: 1,
+            tokens: Vec::new(),
+        }
+    }
+
+    pub fn lex(&mut self) -> Vec<Token> {
+        while let Some(c) = self.next() {
+            match c {
+                '(' => self.emit(Token::LeftBrace),
+                ')' => self.emit(Token::RightBrace),
+                c => {
+                    if c.is_whitespace() {
+                        // skip
+                    } else if c.is_digit(10) {
+                        let mut number_string = c.to_string();
+
+                        while let Some(number) = self.peek().filter(|d| d.is_digit(10)) {
+                            number_string.push(*number);
+                            self.next();
+                        }
+                        // emit a float
+                        if self.peek() == Some(&'.') {
+                            number_string.push('.');
+                            self.next();
+                            while let Some(number) = self.peek().filter(|d| d.is_digit(10)) {
+                                number_string.push(*number);
+                                self.next();
+                            }
+                            self.try_emit_float(number_string);
+                        } else {
+                            // emit int
+                            self.try_emit_int(number_string);
+                        }
+                    }
+                }
+            }
+        }
+        // prevent error
+        Vec::new()
+    }
+
+    fn emit(&mut self, token: Token) {
+        // TODO: DEBUG flag so we only log when in debug
+        println!("Emitting token: {:?}", token);
+        self.tokens.push(token);
+    }
+
+    fn try_emit_float(&mut self, number_string: String) {
+        match number_string.parse::<f64>() {
+            Ok(f) => self.emit(Token::Float(f)),
+            Err(e) => self.emit(Token::Invalid(format!(
+                "lexing: invalid token found: {}",
+                e
+            ))),
+        }
+    }
+
+    fn try_emit_int(&mut self, number_string: String) {
+        match number_string.parse::<i32>() {
+            Ok(f) => self.emit(Token::Int(f)),
+            Err(e) => self.emit(Token::Invalid(format!(
+                "lexing: invalid token found: {}",
+                e
+            ))),
+        }
+    }
+
+    fn next(&mut self) -> Option<char> {
+        // TODO: later needs check to see if this char is a newline
+        self.code.next()
+    }
+
+    fn peek(&mut self) -> Option<&char> {
+        self.code.peek()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::{
+    env,
+    io::{stdin, stdout, Write},
+};
+
+use crate::lexer::Lexer;
+
+pub mod interpreter;
+pub mod lexer;
+
+fn prompt(input: &mut String) -> bool {
+    input.clear();
+    print!("> ");
+    if stdout().flush().is_err() {
+        return false;
+    }
+
+    match stdin().read_line(input) {
+        Err(_) => false,
+        Ok(_) => true,
+    }
+}
+
+fn repl() {
+    let mut input = String::new();
+    while prompt(&mut input) {
+        println!("{}", input);
+        let mut lexer = Lexer::new(&input);
+        lexer.lex();
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() <= 1 {
+        repl();
+    }
 }
