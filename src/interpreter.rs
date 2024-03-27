c41e5280146f1ebae4b53da0119c1c67de713686
Maurits Seelen <mauritsseelen@hotmail.com>
2024-03-27 07:56:04 +0100

    unary

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -3,6 +3,7 @@ use crate::{core::LErr, lexer::Token, parser::Expr};
 #[derive(Debug, Clone)]
 pub enum Obj {
     Null,
+    Bool(bool),
     Num(LNum),
     Seq(Seq),
 }
@@ -10,7 +11,7 @@ pub enum Obj {
 #[derive(Debug, Clone)]
 pub enum LNum {
     Int(i64),
-    Float(f64)
+    Float(f64),
 }
 
 #[derive(Debug, Clone)]
@@ -24,9 +25,10 @@ pub struct Interpreter;
 
 impl Interpreter {
     pub fn new() -> Self {
-        Self { }
+        Self {}
     }
-    
+
+    // TODO: if eval goes wrong, return runtime errors
     pub fn eval(&self, expr: &Expr) -> LRes<Obj> {
         match &expr {
             Expr::Sequence(xs) => {
@@ -39,45 +41,75 @@ impl Interpreter {
                 // sequence is nested
                 self.eval(xs.last().unwrap())
                 //Ok(Obj::Null)
-            },
+            }
             Expr::Int(v) => Ok(Obj::Num(LNum::Int(*v))),
             Expr::Float(v) => Ok(Obj::Num(LNum::Float(*v))),
             Expr::Identifier(v) => Ok(Obj::Seq(Seq::String(v.to_string()))),
-            Expr::Unary(_, _) => todo!(),
+            Expr::Unary(t, v) => {
+                let rvalue = self.eval(v)?;
+
+                match t {
+                    Token::Bang => {
+                        return Ok(Obj::Bool(self.is_truthy(rvalue)));
+                    }
+                    Token::Minus => match self.check_operand(rvalue) {
+                        Ok(o) => return Ok(o), // TODO: negate the value
+                        Err(e) => return Err(e),
+                    },
+                    _ => Ok(Obj::Null),
+                }
+            }
             Expr::Logical(_, _, _) => todo!(),
             Expr::Binary(lv, op, rv) => {
                 let lvalue = self.eval(lv)?;
                 let rvalue = self.eval(rv)?;
+
+                // TODO: make some function in Num that can combined multiple numbers into 1 Obj::Num
                 let real_l_v = match lvalue {
-                    Obj::Num(value) => {
-                        match value {
-                            LNum::Int(i) => i as f64,
-                            LNum::Float(f) => f,
-                        }        
-                    }
-                    _ => 0 as f64 // should be error
+                    Obj::Num(value) => match value {
+                        LNum::Int(i) => i as f64,
+                        LNum::Float(f) => f,
+                    },
+                    _ => 0 as f64, // should be error
                 };
                 let real_r_v = match rvalue {
-                    Obj::Num(value) => {
-                        match value {
-                            LNum::Int(i) => i as f64,
-                            LNum::Float(f) => f,
-                        }        
-                    }
-                    _ => 0 as f64 // should be error
+                    Obj::Num(value) => match value {
+                        LNum::Int(i) => i as f64,
+                        LNum::Float(f) => f,
+                    },
+                    _ => 0 as f64, // should be error
                 };
-               
-                
+
                 match op {
                     Token::Plus => return Ok(Obj::Num(LNum::Float(real_l_v + real_r_v))),
                     Token::Minus => return Ok(Obj::Num(LNum::Float(real_l_v - real_r_v))),
                     Token::Star => return Ok(Obj::Num(LNum::Float(real_l_v * real_r_v))),
                     Token::Slash => return Ok(Obj::Num(LNum::Float(real_l_v / real_r_v))),
-                    _ => return Ok(Obj::Null)
+                    _ => return Ok(Obj::Null),
                 };
-            },
+            }
             Expr::Var(_) => todo!(),
             Expr::Assign(_, _) => todo!(),
+        }
+    }
+
+    fn is_truthy(&self, obj: Obj) -> bool {
+        match obj {
+            Obj::Bool(b) => b,
+            Obj::Null => false,
+            _ => false,
+        }
+    }
+
+    fn check_operand(&self, obj: Obj) -> Result<Obj, LErr> {
+        match obj {
+            Obj::Num(v) => Ok(Obj::Num(v)),
+            _ => {
+                return Err(LErr::parsing_error(
+                    "Operand must be a number.".to_string(),
+                    1,
+                ))
+            }
         }
     }
 }
